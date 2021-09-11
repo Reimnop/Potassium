cbe4b8a5bce106782338b13cd668e2c30ecaeb60
Reimnop <56994757+Reimnop@users.noreply.github.com>
2021-09-11 14:21:55 +0700

    Added loads of comments

--- GameManagerPatcher.cs
+++ GameManagerPatcher.cs
@@ -12,9 +12,11 @@ namespace Potassium
         {
             List<DataManager.GameData.BeatmapObject> beatmapObjects = DataManager.inst.gameData.beatmapObjects;
 
+            // We add all the objects to the dictionary for fast lookup later on.
             ObjectManagerPatcher.BeatmapObjectsLookup = new Dictionary<string, DataManager.GameData.BeatmapObject>();
             foreach (DataManager.GameData.BeatmapObject beatmapObject in beatmapObjects)
             {
+                // No TryAdd :(
                 if (!ObjectManagerPatcher.BeatmapObjectsLookup.ContainsKey(beatmapObject.id))
                 {
                     ObjectManagerPatcher.BeatmapObjectsLookup.Add(beatmapObject.id, beatmapObject);
--- ObjectManagerPatcher.cs
+++ ObjectManagerPatcher.cs
@@ -53,6 +53,8 @@ namespace Potassium
         [HarmonyPrefix]
         public static bool UpdatePrefix()
         {
+            // Return true = don't skip original method
+            // Since Editor is not implemented, we skip this pipeline and use the old pipeline instead.
             if (EditorManager.inst != null && EditorManager.inst.isEditing)
                 return true;
 
@@ -63,8 +65,8 @@ namespace Potassium
 
             float timeDelta = time - lastAudioTime;
 
-            //do actions on objects
-            if (timeDelta >= 0f) // forward
+            // We have saved all the objects into one array called ObjectActions in order to always have in order spawn and despawn.
+            if (timeDelta >= 0f) // If time is going in forward direction
             {
                 while (index < objectActions.Count && objectActions[index].Time < time)
                 {
@@ -82,7 +84,7 @@ namespace Potassium
                     index++;
                 }
             }
-            else //reverse
+            else // If time is going in backward direction
             {
                 while (index > 0 && objectActions[index - 1].Time >= time)
                 {
@@ -103,21 +105,22 @@ namespace Potassium
 
             lastAudioTime = time;
 
-            //multithreaded update
+            // We update the objects asynchronously. This is entire optional and can be removed.
             int workersCount = ThreadManager.NumberOfWorkers;
             for (int i = 0; i < workersCount; i++)
             {
                 int workerIndex = i;
                 ThreadManager.QueueWorker(i, () =>
                 {
-                    //update objects
+                    // Update objects.
+                    // Each thread process a different subset of object.
                     for (int j = workerIndex; j < aliveObjects.Count; j += workersCount)
                     {
                         LevelObjectRef levelObject = aliveObjects[j];
 
                         DataManager.GameData.BeatmapObject beatmapObject = levelObject.BeatmapObject;
 
-                        //update object color
+                        // Update the object's color
                         ColorSequence col = levelObject.ColorSequence;
                         if (beatmapObject.objectType != DataManager.GameData.BeatmapObject.ObjectType.Empty && levelObject.ObjectMaterial != null)
                         {
@@ -136,7 +139,7 @@ namespace Potassium
                             }
                         }
 
-                        //update object transform
+                        // Now the transform
                         Sequence pos = levelObject.PositionSequence;
                         Sequence sca = levelObject.ScaleSequence;
                         Sequence rot = levelObject.RotationSequence;
@@ -153,7 +156,8 @@ namespace Potassium
                         levelObject.BaseTransform.localScale = new Vector3(scaValues[0], scaValues[1], 1f);
                         levelObject.BaseTransform.localEulerAngles = new Vector3(0f, 0f, rotValues[0]);
 
-                        //update parent transforms
+                        // Update the parents' transform
+                        // I don't actually know how parent offset works so it might be broken.
                         for (int k = levelObject.Parents.Count - 1; k >= 0; k--)
                         {
                             ParentObjectRef parentObject = levelObject.Parents[k];
@@ -191,13 +195,14 @@ namespace Potassium
                 });
             }
 
-            //start
+            // Now start all the object's processing
             ThreadManager.StartAllWorkers();
 
-            //wait until all done
+            // Spinwait until everything is done
             while (!ThreadManager.AllDone()) ;
 
-            return false; //skip original method
+            return false; // Return false to skip original method
+            // This method basically replaces the entire old pipeline and replace it with a more optimized version
         }
 
         public static void InitLevel()
@@ -214,6 +219,7 @@ namespace Potassium
 
             foreach (DataManager.GameData.BeatmapObject beatmapObject in BeatmapObjectsLookup.Values)
             {
+                // We preprocess the sequence in order to get consistent random values
                 sequencesLookup.Add(beatmapObject.id, new SequencesPair
                 {
                     ObjectStartTime = beatmapObject.StartTime,
@@ -224,8 +230,13 @@ namespace Potassium
                 });
             }
 
+            // For every object, we initialize the entire object tree.
+            // We spawn everything at start, with everything being inactive.
+            // Setting a bool is definitely way faster than instantiate and/or pooling, and according to many benchmarks, it also doesn't take a lot of memory.
+            // Inactive objects has so little performance penalty it's negligible.
             foreach (DataManager.GameData.BeatmapObject beatmapObject in BeatmapObjectsLookup.Values)
             {
+                // Since empty objects are not visible anyway, there is no point initializing its tree.
                 if (beatmapObject.objectType != DataManager.GameData.BeatmapObject.ObjectType.Empty)
                 {
                     InitObjectTree(beatmapObject);
@@ -235,7 +246,8 @@ namespace Potassium
             sequencesLookup.Clear();
             BeatmapObjectsLookup.Clear();
 
-            //put all objects into actions
+            // Now put all the objects in action
+            // Note that we add a small amount to kill time, this is to ensure that kill time is always > start time.
             for (int i = 0; i < levelObjects.Count; i++)
             {
                 LevelObjectRef levelObject = levelObjects[i];
@@ -249,18 +261,19 @@ namespace Potassium
 
                 objectActions.Add(new LevelObjectAction
                 {
-                    Time = levelObject.KillTime,
+                    Time = levelObject.KillTime + 0.0001f,
                     ActionType = ObjectActionType.Kill,
                     LevelObject = levelObject
                 });
             }
 
-            //sort actions
+            // Now sort all the actions
             objectActions.Sort((x, y) => x.Time.CompareTo(y.Time));
         }
 
         private static void InitObjectTree(DataManager.GameData.BeatmapObject beatmapObject)
         {
+            // We recursively loop back to the top of the tree and start from the top of the tree.
             List<ParentObjectRef> parents = new List<ParentObjectRef>();
             Transform parent = ObjectManager.inst.objectParent.transform;
             if (BeatmapObjectsLookup.TryGetValue(beatmapObject.parent, out var value))
@@ -276,7 +289,7 @@ namespace Potassium
 
             SequencesPair sequences = sequencesLookup[beatmapObject.id];
 
-            //we deep copy because we don't want the same instance of sequence being accessed at the same time on different threads
+            // We deep copy because we don't want the same instance of sequence being accessed at the same time on different threads
             Sequence posSeq = sequences.PositionSequence.DeepCopy();
             Sequence scaSeq = sequences.ScaleSequence.DeepCopy();
             Sequence rotSeq = sequences.RotationSequence.DeepCopy();
@@ -289,6 +302,7 @@ namespace Potassium
             float[] scaValues = scaSeq.GetValues();
             float[] rotValues = rotSeq.GetValues();
 
+            // Set the transforms in advance to account for objects that spawn and despawn immediately
             baseTransform.localPosition = new Vector3(posValues[0], posValues[1], beatmapObject.Depth * 0.1f);
             baseTransform.localScale = new Vector3(scaValues[0], scaValues[1], 1f);
             baseTransform.localEulerAngles = new Vector3(0f, 0f, rotValues[0]);
@@ -376,7 +390,7 @@ namespace Potassium
 
             SequencesPair sequences = sequencesLookup[beatmapObject.id];
 
-            //we deep copy because we don't want the same instance of sequence being accessed at the same time on different threads
+            // We deep copy because we don't want the same instance of sequence being accessed at the same time on different threads
             Sequence posSeq = sequences.PositionSequence.DeepCopy();
             Sequence scaSeq = sequences.ScaleSequence.DeepCopy();
             Sequence rotSeq = sequences.RotationSequence.DeepCopy();
@@ -389,6 +403,7 @@ namespace Potassium
             float[] scaValues = scaSeq.GetValues();
             float[] rotValues = rotSeq.GetValues();
 
+            // Set the transforms in advance to account for objects that spawn and despawn immediately
             currentTransform.localPosition = animPos ? new Vector3(posValues[0], posValues[1], baseBeatmapObject.Depth * 0.0005f) : new Vector3(0f, 0f, baseBeatmapObject.Depth * 0.0005f);
             currentTransform.localScale = animSca ? new Vector3(scaValues[0], scaValues[1], 1f) : Vector3.one;
             currentTransform.localEulerAngles = animRot ? new Vector3(0f, 0f, rotValues[0]) : Vector3.zero;
